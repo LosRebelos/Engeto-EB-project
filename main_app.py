e9ebd71e26d18122b7a78217c3077b9aa522ea07
LosRebelos <95313998+LosRebelos@users.noreply.github.com>
2022-07-30 15:22:36 +0200

    engine to cloud db

--- main_app.py
+++ main_app.py
@@ -4,12 +4,12 @@ import pandas as pd
 import pymysql
 import streamlit as st
 
-engine = create_engine("mysql+pymysql://root:kokos123@localhost:3306/test")
+engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
 df_bikes = pd.read_sql(sql=
 '''SELECT
 	start_station_name,
 	start_station_latitude as lat,
-	start_station_longitude as lon,
+	start_station_longitude as lon
 FROM edinburgh_bikes eb
 GROUP BY start_station_name
 ORDER BY COUNT(*) DESC;'''
