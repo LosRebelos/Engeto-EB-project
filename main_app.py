778705b2dbf1370c6d9c2a1798dc161ff9cdd380
LosRebelos <95313998+LosRebelos@users.noreply.github.com>
2022-07-30 18:33:55 +0200

    Original with two dfs and two layers

--- main_app.py
+++ main_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pydeck as pdk
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
-df = pd.read_sql(sql=
+df_bikes_active = pd.read_sql(sql=
 '''WITH base AS (
 	SELECT
 		start_station_name,
@@ -21,13 +21,28 @@ SELECT
 	lat,
 	lon,
 	number_of_rents
-FROM base;'''
+FROM base
+WHERE number_of_rents > 200;'''
 , con=engine)
 
-df.loc[df['number_of_rents'] > 200, 'Status'] = 'Active'
-df.loc[df['number_of_rents'] <= 200, 'Status'] = 'Inactive'
-df.loc[df['Status'] == 'Active', 'Color'] = '[0, 255, 0, 160]'
-df.loc[df['Status'] == 'Inactive', 'Color'] = '[255, 0, 0, 160]'
+df_bikes_inactive = pd.read_sql(sql=
+'''WITH base AS (
+	SELECT
+		start_station_name,
+		start_station_latitude as lat,
+		start_station_longitude as lon,
+		COUNT(*) AS number_of_rents
+	FROM edinburgh_bikes eb
+	GROUP BY start_station_name
+)
+SELECT
+	start_station_name,
+	lat,
+	lon,
+	number_of_rents
+FROM base
+WHERE number_of_rents <= 200;'''
+, con=engine)
 
 st.set_page_config(layout="wide")
 st.title('Edinburgh bikes project')
@@ -50,9 +65,16 @@ if page == 'Mapa':
 			layers = [
 				pdk.Layer(
 					"ScatterplotLayer",
-					df,
+					df_bikes_active,
 					get_position=['lon', 'lat'],
-					get_fill_color="Color",
+					get_fill_color=[0, 255, 0, 160],
+					get_radius=30
+				),
+				pdk.Layer(
+					"ScatterplotLayer",
+					df_bikes_inactive,
+					get_position=['lon', 'lat'],
+					get_fill_color=[255, 0, 0, 160],
 					get_radius=30
 				),
 				]
