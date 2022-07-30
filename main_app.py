2fdc234bcc2d6e3a1b72b396d9743c77158c5970
LosRebelos <95313998+LosRebelos@users.noreply.github.com>
2022-07-30 15:43:31 +0200

    Map setup

--- main_app.py
+++ main_app.py
@@ -23,7 +23,7 @@ st.pydeck_chart(
             latitude= 55.95233546161639,
             longitude= -3.207101172107286,
             zoom=50,
-            pitch=50
+            pitch=10
         ),
         layers = [
             pdk.Layer(
