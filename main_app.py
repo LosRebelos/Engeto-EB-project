9d7fd2da13b81fdfcec95962ae42f5cb3a5dd7fe
LosRebelos <95313998+LosRebelos@users.noreply.github.com>
2022-07-30 15:29:16 +0200

    Pydeck try

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@ import geopy.distance as gd
 import pandas as pd
 import pymysql
 import streamlit as st
+import pydeck as pdk
 
 engine = create_engine("mysql+pymysql://data-student:u9AB6hWGsNkNcRDm@data.engeto.com:3306/data_academy_04_2022")
 df_bikes = pd.read_sql(sql=
@@ -15,4 +16,24 @@ GROUP BY start_station_name
 ORDER BY COUNT(*) DESC;'''
 , con=engine)
 
-st.map(df_bikes)
+st.pydeck_chart(
+    pdk.Deck(
+        map_style='mapbox://styles/mapbox/light-v9',
+        initial_view_state=pdk.ViewState(
+            latitude=-17,
+            longitude=-65,
+            zoom=4,
+            pitch=50
+        ),
+        layers = [
+            pdk.Layer(
+                "ScatterplotLayer",
+                df_bikes,
+                get_position=['lon', 'lat'],
+                get_fill_color=['r', 'g', 'b'],
+                get_line_color=[0, 0, 0],
+                get_radius=100000
+            )
+        ]
+    )
+)
